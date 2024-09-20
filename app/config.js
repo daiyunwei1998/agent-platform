6f2f54863a27118e7a9a486bfbef02a110800497
Yunwei Dai <daiyunwei1998@gmail.com>
2024-09-20 17:25:06 +0800

    refactor: Update tenantServiceHost URL in config.js

--- app/config.js
+++ app/config.js
@@ -1,2 +1,2 @@
 export const chatServiceHost = "http://localhost:8080"; // process.env.NEXT_PUBLIC_CHAT_SERVICE_HOST || "http://localhost:8080";
-export const ternantServiceHost = "http://localhost:8000"; 
+export const tenantServiceHost = "http://localhost:8000"; 
