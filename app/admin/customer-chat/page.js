b83634b489455f2ab6b54e30091121e025607f4b
Yunwei Dai <daiyunwei1998@gmail.com>
2024-09-20 17:27:24 +0800

    feat: Add customer chat page component
    Add a new page component for the customer chat feature. This component includes the AgentChat component for displaying chat messages.

---
+++ app/admin/customer-chat/page.js
@@ -0,0 +1,10 @@
+import AgentChat from '@/app/components/AgentChat'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <AgentChat></AgentChat>
+    </div>
+  )
+}
