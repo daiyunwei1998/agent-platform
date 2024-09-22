12544d632c418661026a4de8de87cf0d1fa299c7
Yunwei Dai <daiyunwei1998@gmail.com>
2024-09-23 05:35:29 +0800

    fix: fix mising import in bot management page

--- app/(root layout)/admin/bot-management/page.js
+++ app/(root layout)/admin/bot-management/page.js
@@ -1,6 +1,6 @@
 import Dashboard from '@/app/components/Dashboard'
 import React from 'react'
-import { CookiesProvider } from 'react-cookie'
+import { cookies } from 'next/headers'
 
 export default function page() {
     const tenantId = cookies().get('tenantId')?.value
