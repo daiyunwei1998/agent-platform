03efe13f83a1c508d291624b94860dac46ca5fcb
Yunwei Dai <daiyunwei1998@gmail.com>
2024-09-24 09:17:59 +0800

    chore: reset host for demo purposes

--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 const domain = "203.204.185.67";
 
-export const chatServiceHost = `http://${domain}:8080`; 
-export const tenantServiceHost = `http://${domain}:8000`;
+export const chatServiceHost = `http://${domain}:8080`;  //`http://localhost:8080`;
+export const tenantServiceHost = `http://${domain}:8000`; //`http://localhost:8000`;
 export const aiServiceHost = `http://${domain}:8001`;
 export const host = `http://${domain}:3000`;
+export const imageHost = "https://d5aepzwh87at2.cloudfront.net";
